cdaa1b5dc79425eaad4cfa684571cd275052c225
Andžej <andrzej.korovacki@gmail.com>
2022-07-05 13:30:34 +0300

    toPolish method exported

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { convertNumber as convertNumberToEnglish } from "./english/converter";
+import { convertNumber as convertNumberToPolish } from "./polish/converter";
 
 export function toEnglish(number: number): string {
   return convertNumberToEnglish(number);
+}
+
+export function toPolish(number: number): string {
+  return convertNumberToPolish(number);
 }
