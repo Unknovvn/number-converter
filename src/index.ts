cc705ff7e48c11f0652ae85b070ec7e8bd9acef1
Andžej <andrzej.korovacki@gmail.com>
2022-07-06 12:40:06 +0300

    toLithuanian exported from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { convertNumber as convertNumberToEnglish } from "./english/converter";
 import { convertNumber as convertNumberToPolish } from "./polish/converter";
+import { convertNumber as convertNumberToLithuanian } from "./lithuanian/converter";
 
 export function toEnglish(number: number): string {
   return convertNumberToEnglish(number);
@@ -7,4 +8,8 @@ export function toEnglish(number: number): string {
 
 export function toPolish(number: number): string {
   return convertNumberToPolish(number);
+}
+
+export function toLithuanian(number: number): string {
+  return convertNumberToLithuanian(number);
 }
